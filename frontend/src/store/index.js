598cb2fdac1f787919e57983ec23fe9ac690bf03
Eric Quan <equan1090@gmail.com>
2021-09-10 16:18:50 -0600

    finished session action

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import sessionReducer from './session';
 
-
-
-
+const rootReducer = combineReducers({
+  session: sessionReducer,
+});
 let enhancer;
 
 if (process.env.NODE_ENV === 'production') {
@@ -16,8 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-const rootReducer = combineReducers({
-});
+
 
 const configureStore = (preloadedState) => {
     return createStore(rootReducer, preloadedState, enhancer);
