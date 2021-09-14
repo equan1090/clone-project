c436c7baee0860679ed39eef8ce24583c8eb170d
Eric Quan <equan1090@gmail.com>
2021-09-14 17:20:34 -0600

    created a backend to add and delete songs from database

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -6,10 +6,25 @@ const {Song} = require("../../db/models")
 const router = express.Router();
 
 
-// router.get('/')
+router.get('/', asyncHandler(async(req, res) => {
+    const songs = await Song.findAll();
+    res.json(songs)
+}))
+router.get('/:songId', asyncHandler(async(req, res) => {
+    const id = req.params
+    const songs = await Song.findByPk(id.songId);
+    res.json(songs)
+}))
 
 router.post('/', asyncHandler(async(req, res) => {
     const song = await Song.create(req.body);
+    res.json(song)
+}))
+
+router.delete('/:songId', asyncHandler(async(req, res) => {
+    const id = req.params
+    const song = await Song.findByPk(id.songId)
+    await song.destroy();
     res.json(song)
 }))
 
